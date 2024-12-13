fc2c665aff0254dfed84b80943339106172c5716
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2024-12-12 20:06:58 -0600

    fix: test

---
+++ .next/.next/server/pages/_document.js
@@ -0,0 +1,45 @@
+"use strict";(()=>{var e={};e.id=660,e.ids=[660],e.modules={6840:(e,t,s)=>{s.r(t),s.d(t,{default:()=>o});var n=s(997),a=s(3590),r=s(3218),i=s.n(r);function o(){let e="CO-FUN",t="Recruit AI assistant team for your business in seconds.",s="https://images.prismic.io/syntia/Zvx8DrVsGrYSwQnL_CO-FUN.png?auto=format,compress";return(0,n.jsxs)(a.Html,{lang:"en",children:[(0,n.jsxs)(a.Head,{children:[n.jsx("meta",{name:"robots",content:"follow, index"}),n.jsx("meta",{name:"description",content:t}),n.jsx("meta",{property:"og:site_name",content:e}),n.jsx("meta",{property:"og:description",content:t}),n.jsx("meta",{property:"og:title",content:e}),n.jsx("meta",{property:"og:image",content:s}),n.jsx("meta",{name:"twitter:card",content:"summary_large_image"}),n.jsx("meta",{name:"twitter:site",content:"@yourname"}),n.jsx("meta",{name:"twitter:title",content:e}),n.jsx("meta",{name:"twitter:description",content:t}),n.jsx("meta",{name:"twitter:image",content:s}),n.jsx("link",{rel:"preconnect",href:"https://fonts.googleapis.com"}),n.jsx("link",{rel:"preconnect",href:"https://fonts.gstatic.com"}),n.jsx("link",{href:"https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",rel:"stylesheet"}),n.jsx("link",{href:"https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",rel:"stylesheet"}),n.jsx("link",{href:"https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap",rel:"stylesheet"}),n.jsx("link",{href:"https://fonts.googleapis.com/css2?family=Sen:wght@400..800&display=swap",rel:"stylesheet"}),n.jsx("script",{type:"text/javascript",src:"//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"}),n.jsx(i(),{id:"jquery-custom-script",type:"text/javascript",strategy:"beforeInteractive",dangerouslySetInnerHTML:{__html:`
+                (function($) {
+                  window.fnames = new Array();
+                  window.ftypes = new Array();
+                  fnames[0] = 'EMAIL';
+                  ftypes[0] = 'email';
+                  fnames[1] = 'FNAME';
+                  ftypes[1] = 'text';
+                  fnames[2] = 'LNAME';
+                  ftypes[2] = 'text';
+                  fnames[3] = 'ADDRESS';
+                  ftypes[3] = 'address';
+                  fnames[4] = 'PHONE';
+                  ftypes[4] = 'phone';
+                  fnames[5] = 'BIRTHDAY';
+                  ftypes[5] = 'birthday';
+                  fnames[6] = 'COMPANY';
+                  ftypes[6] = 'text';
+                  fnames[7] = 'MESSAGE';
+                  ftypes[7] = 'text';
+                }(jQuery));
+                var $mcj = jQuery.noConflict(true);
+              `}}),n.jsx(i(),{id:"mailchimp-initialization",type:"text/javascript",strategy:"beforeInteractive",dangerouslySetInnerHTML:{__html:`
+                (function($) {
+                  window.fnames = new Array();
+                  window.ftypes = new Array();
+                  fnames[0] = 'EMAIL';
+                  ftypes[0] = 'email';
+                  fnames[1] = 'FNAME';
+                  ftypes[1] = 'text';
+                  fnames[2] = 'LNAME';
+                  ftypes[2] = 'text';
+                  fnames[3] = 'ADDRESS';
+                  ftypes[3] = 'address';
+                  fnames[4] = 'PHONE';
+                  ftypes[4] = 'phone';
+                  fnames[5] = 'BIRTHDAY';
+                  ftypes[5] = 'birthday';
+                  fnames[6] = 'COMPANY';
+                  ftypes[6] = 'text';
+                  fnames[7] = 'MESSAGE';
+                  ftypes[7] = 'text';
+                }(jQuery));
+                var $mcj = jQuery.noConflict(true);
+              `}})]}),(0,n.jsxs)("body",{children:[n.jsx(a.Main,{}),n.jsx(a.NextScript,{})]})]})}},2785:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{e.exports=require("react")},6405:e=>{e.exports=require("react-dom")},997:e=>{e.exports=require("react/jsx-runtime")},1017:e=>{e.exports=require("path")}};var t=require("../webpack-runtime.js");t.C(e);var s=e=>t(t.s=e),n=t.X(0,[928,571],()=>s(6840));module.exports=n})();
